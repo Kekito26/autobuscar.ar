1f4bf23a854dc84f5eda29b3d2600fff6508410f
Kekito26 <damianrombola1@gmail.com>
2026-05-10 19:34:00 -0300

    added CI/CD and tests

---
+++ tests/example.spec.js
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach('login', async ({ page }) => {
+  await page.goto('https://autobuscar-ar-web.vercel.app/');
+  await page.getByTestId('login-trigger').click();
+  await page.getByTestId('login-username-input').click();
+  await page.getByTestId('login-username-input').fill('admin');
+  await page.getByTestId('login-username-input').press('Tab');
+  await page.getByTestId('login-password-input').fill('testing123');
+  await page.getByTestId('login-submit').click();
+  //await page.pause();
+  await page.getByTestId('user-menu-trigger').click();
+  await expect(page.getByTestId('user-menu-profile')).toBeVisible();
+  await page.pause();
+});
+
+test('user search', async ({ page }) => {
+  await page.goto('https://autobuscar-ar-web.vercel.app/');
+  await page.getByTestId('hero-cta-search').click();
+  await expect(page.getByTestId('search-input')).toBeVisible();
+});
+
